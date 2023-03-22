293a1af12b716d68a6db2cf272e9b3a181638594
elisa-he <97122439+elisa-he@users.noreply.github.com>
2023-03-21 21:32:56 -0700

    work page in progress

--- src/page/Work.jsx
+++ src/page/Work.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import logo from "../image/.eh.svg";
+import works from "../content-json/work.json";
 export default function Work() {
   return (
     <>
@@ -10,6 +11,20 @@ export default function Work() {
         <div className="highlight">
           <h2 className="uppercase green">Works</h2>
         </div>
+
+        {works &&
+          works.map((work, i) => {
+            return (
+              <div className="workWrap" key={i}>
+                <h3>{work.title}</h3>
+                {work.usedSkill.slice(0, 3).map((skill, i) => (
+                  <p key={i} className="skill">
+                    {skill}
+                  </p>
+                ))}
+              </div>
+            );
+          })}
       </section>
       <Navbar color="mainColor" />
       <Footer />
